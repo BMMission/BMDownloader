5f9ee722a8e1a00806367cb5abba87f9afce4b20
BMMision <rezagina68@gmail.com>
2024-11-18 05:48:43 +0330

    added some changes in bindings exports

--- src/export/bindings.rs
+++ src/export/bindings.rs
@@ -8,10 +8,25 @@ use std::os::raw::c_char;
 use std::os::raw::c_int;
 use std::path::Path;
 use std::{env, fs, thread};
+use directories::UserDirs;
+#[no_mangle]
+pub extern  "C" fn default_folder_c(path:*const c_char)->bool {
+    
+    if let Some(user_dirs) = UserDirs::new() {
+        let download_path= user_dirs.download_dir().unwrap();
+        match env::set_current_dir(download_path) {
+            Ok(_) =>true,
+            Err(_) =>false
+        }
+    }else{
+        false
+    }
+    
+}
+
 #[no_mangle]
 pub extern  "C" fn is_data_exists_c(path:*const c_char)->bool {
     let binding = unsafe {
-        assert!(!path.is_null());
         CStr::from_ptr(path).to_string_lossy().into_owned()
     }.clone();
     let download_path = binding.as_str();
@@ -41,13 +56,38 @@ pub extern  "C" fn change_download_folder_c(path:*const c_char){
 }
 #[no_mangle]
 pub extern "C" fn get_downloads_c(limits: c_int, sort_by: *const c_char) -> *const c_char {
-    let sort_by_str = unsafe {
-        assert!(!sort_by.is_null());
-        CStr::from_ptr(sort_by).to_string_lossy().into_owned()
+    let sort_by_str = unsafe { CStr::from_ptr(sort_by).to_str().unwrap_or("") };
+
+    let result = downloader::get_downloads(limits as i32, sort_by_str);
+    CString::new(result).unwrap().into_raw()
+}
+#[no_mangle]
+pub extern "C" fn add_new_download_general_c(
+    url: *const c_char, 
+    output_path: *const c_char, 
+) -> c_int {
+    let url_str = unsafe {
+        CStr::from_ptr(url).to_string_lossy().into_owned()
     };
 
-    let result = downloader::get_downloads(limits as i32, &sort_by_str);
-    CString::new(result).unwrap().into_raw()
+    let output_path_str = unsafe {
+        CStr::from_ptr(output_path).to_string_lossy().into_owned()
+    };
+    let types_str = CString::new("part".to_string()).to_owned().unwrap().to_string_lossy().into_owned();
+    let version_str = CString::new("1.0.0".to_string()).to_owned().unwrap().to_string_lossy().into_owned();
+    let category_str = CString::new("general".to_string()).to_owned().unwrap().to_string_lossy().into_owned();
+    let divise_str = CString::new("general".to_string()).to_owned().unwrap().to_string_lossy().into_owned();
+
+    let result = downloader::add_new_download(
+        &url_str, 
+        &output_path_str, 
+        &types_str, 
+        &version_str, 
+        &category_str, 
+        &divise_str
+    );
+
+    result
 }
 
 #[no_mangle]
@@ -59,38 +99,32 @@ pub extern "C" fn add_new_download_c(
     category: *const c_char, 
     divise: *const c_char
 ) -> c_int {
-    // Convert the C-style strings to Rust strings
+
     let url_str = unsafe {
-        assert!(!url.is_null());
         CStr::from_ptr(url).to_string_lossy().into_owned()
     };
 
     let output_path_str = unsafe {
-        assert!(!output_path.is_null());
         CStr::from_ptr(output_path).to_string_lossy().into_owned()
     };
 
     let types_str = unsafe {
-        assert!(!types.is_null());
         CStr::from_ptr(types).to_string_lossy().into_owned()
     };
 
     let version_str = unsafe {
-        assert!(!version.is_null());
         CStr::from_ptr(version).to_string_lossy().into_owned()
     };
 
     let category_str = unsafe {
-        assert!(!category.is_null());
         CStr::from_ptr(category).to_string_lossy().into_owned()
     };
 
     let divise_str = unsafe {
-        assert!(!divise.is_null());
         CStr::from_ptr(divise).to_string_lossy().into_owned()
     };
 
-    // Now call the Rust function with the extracted parameters
+    // Now call the Rust function wreith the extracted parameters
     let result = downloader::add_new_download(
         &url_str, 
         &output_path_str, 
@@ -99,16 +133,20 @@ pub extern "C" fn add_new_download_c(
         &category_str, 
         &divise_str
     );
-
-    // Return the result as an integer (return type c_int)
     result
 }
-
 #[no_mangle]
 pub extern "C" fn ready_to_download_queue_c(id: c_int) -> bool {
     downloader::ready_to_download_queue(id as i32)
 }
-
+#[no_mangle]
+pub extern "C" fn remove_download_by_id_c(id: c_int) -> bool {
+    downloader::remove_download_by_id(id as i32)
+}
+#[no_mangle]
+pub extern "C" fn remove_schedule_by_id_c(id: c_int) -> bool {
+    schedules::remove_schedule_by_id(id as i32)
+}
 #[no_mangle]
 pub extern "C" fn pause_download_by_id_c(id: c_int) {
     downloader::pause_download_by_id(id as i32);
@@ -117,7 +155,6 @@ pub extern "C" fn pause_download_by_id_c(id: c_int) {
 #[no_mangle]
 pub extern "C" fn get_file_info_c(id: c_int, output_path: *const c_char) -> *const c_char {
     let output_path_str = unsafe {
-        assert!(!output_path.is_null());
         CStr::from_ptr(output_path).to_string_lossy().into_owned()
     };
 
@@ -129,7 +166,6 @@ pub extern "C" fn get_file_info_c(id: c_int, output_path: *const c_char) -> *con
 #[no_mangle]
 pub extern "C" fn get_schedules_c(limits: c_int, sort_by: *const c_char) -> *const c_char {
     let sort_by_str = unsafe {
-        assert!(!sort_by.is_null());
         CStr::from_ptr(sort_by).to_string_lossy().into_owned()
     };
 
@@ -140,17 +176,14 @@ pub extern "C" fn get_schedules_c(limits: c_int, sort_by: *const c_char) -> *con
 #[no_mangle]
 pub extern "C" fn add_new_schedules_c(download_id: c_int, time: *const c_char, etime: *const c_char, day: *const c_char) -> c_int {
     let time_str = unsafe {
-        assert!(!time.is_null());
         CStr::from_ptr(time).to_string_lossy().into_owned()
     };
 
     let etime_str = unsafe {
-        assert!(!etime.is_null());
         CStr::from_ptr(etime).to_string_lossy().into_owned()
     };
 
     let day_str = unsafe {
-        assert!(!day.is_null());
         CStr::from_ptr(day).to_string_lossy().into_owned()
     };
 
@@ -166,12 +199,10 @@ pub extern "C" fn active_current_schedules_c() -> *const c_char {
 #[no_mangle]
 pub extern "C" fn update_setting_c(key: *const c_char, value: *const c_char) {
     let key_str = unsafe {
-        assert!(!key.is_null());
         CStr::from_ptr(key).to_string_lossy().into_owned()
     };
 
     let value_str = unsafe {
-        assert!(!value.is_null());
         CStr::from_ptr(value).to_string_lossy().into_owned()
     };
 
@@ -181,7 +212,6 @@ pub extern "C" fn update_setting_c(key: *const c_char, value: *const c_char) {
 #[no_mangle]
 pub extern "C" fn get_setting_c(key: *const c_char) -> *const c_char {
     let key_str = unsafe {
-        assert!(!key.is_null());
         CStr::from_ptr(key).to_string_lossy().into_owned()
     };
 
